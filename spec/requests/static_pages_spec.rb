19d1a0f263154a6b5434965389b97c2d416d98a7
大城由晃 <yoshiaki19980217@icloud.com>
2022-08-28 18:20:51 +0900

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "StaticPages", type: :request do
     it "returns http success" do
       get "/static_pages/home"
       expect(response).to have_http_status(:success)
+      expect(response.body).to include "Home"
     end
   end
 
@@ -12,6 +13,7 @@ RSpec.describe "StaticPages", type: :request do
     it "returns http success" do
       get "/static_pages/help"
       expect(response).to have_http_status(:success)
+      expect(response.body).to include "Help"
     end
   end
 
@@ -19,7 +21,15 @@ RSpec.describe "StaticPages", type: :request do
     it "returns http success" do
       get "/static_pages/about"
       expect(response).to have_http_status(:success)
+      expect(response.body).to include "About"
     end
   end
 
+  describe "GET /" do
+    it "returns http success" do
+      get "/"
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include "Home | Ruby on Rails Tutorial Sample App"
+    end
+  end
 end
