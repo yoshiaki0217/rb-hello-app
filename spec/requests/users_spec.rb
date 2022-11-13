696a134b13c74e7845eb4ff77f1498122dcde3a4
大城由晃 <yoshiaki19980217@icloud.com>
2022-11-13 19:04:52 +0900

    第5章

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   describe "GET /new" do
     it "returns http success" do
-      get "/signup"
+      get "/users/new"
       expect(response).to have_http_status(:success)
     end
   end
